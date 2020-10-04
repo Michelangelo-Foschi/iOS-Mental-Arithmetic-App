82665f1b1ed8babc663a17212a3c2a8bbad1c666
Michelangelo Foschi <michelangelo.foschi.10@gmail.com>
2020-10-04 22:24:01 +0400

    Xcode Project Upload

---
+++ KopfRechnenByMF/KopfRechnenByMF/DataPresentingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DataPresentingViewController.swift
+//  KopfRechnenByMF
+//
+//  Created by Michelangelo Foschi on 02/10/2020.
+//  Copyright © 2020 Michelangelo Foschi. All rights reserved.
+//
+
+import UIKit
+
+class DataPresentingViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var dataImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func donePressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+
+}
